d6dc4d697f040a0a00590d22b3234d774f66f210
Giovanni <haddadi.giovanni@gmail.com>
2020-03-01 14:45:40 +0100

    les tables se créent

--- web/src/main/java/web/Initializer.java
+++ web/src/main/java/web/Initializer.java
@@ -2,10 +2,24 @@ package web;
 
 import core.config.AppConfig;
 import core.config.DBConfig;
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import web.config.WSConfig;
+import web.config.WebConfig;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("cxfServlet", new CXFServlet());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/api/*");
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -14,7 +28,7 @@ public class Initializer extends AbstractAnnotationConfigDispatcherServletInitia
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class<?>[] {WebConfig.class};
     }
 
     @Override
